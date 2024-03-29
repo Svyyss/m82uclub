ad607ec7b736bcaa0c797e121f9fafcc9bd8dd3c
Svyyss <165024794+Svyyss@users.noreply.github.com>
2024-03-29 10:44:16 +0900

    Add files via upload

---
+++ M82U/static/login/JS/Core.js
@@ -0,0 +1,196 @@
+﻿/*
+* Created 20091224@Lee - Core javascript functions and classes
+*
+* !!!CAUTION: ANY REVISION MUST BE APPROVED BEFORE PROCEEDING!!!
+* Revision ?@? - ...
+*
+*/
+function $(id) {
+    return document.getElementById(id);
+}
+// Returns the string with all the beginning
+// and ending whitespace removed
+String.prototype.trim = function () {
+    return this.replace(/^\s+/, '').replace(/\s+$/, '');
+};
+
+function SetParameterValue(name, value, url) {
+    var newParameter = (value != null) ? (name + "=" + value) : null;
+    if (!url.contains("?")) {
+        url = (newParameter == null) ? url : (url + "?" + newParameter);
+        return url;
+    }
+    // Already contain ?
+    var separator = url.contains("?" + name + "=") ? "?" : "&";
+    if (!url.contains("&" + name + "=") && !url.contains("?" + name + "=")) {
+        url = (newParameter == null) ? url : (url + "&" + newParameter);
+        return url;
+    } else {
+        var i1 = url.indexOf(separator + name + "=");
+        var tmp = url.substr(i1);
+        var i2 = tmp.indexOf("&", 1);
+        var oldParameter = i2 >= 0 ? url.substr(i1, i2) : tmp;
+        url = url.replace(oldParameter, (newParameter == null) ? null : (separator + newParameter));
+        return url;
+    }
+}
+
+function GetParameterValue(name, url) {
+    if (null == url || null == name || 0 == name.length) return null;
+    var urlToCompare = url.toLowerCase();
+    name = name.toLowerCase();
+    var separator = urlToCompare.contains("?" + name + "=") ? "?" : "&";
+    var parameter = separator + name + "=";
+    var i1 = urlToCompare.indexOf(parameter);
+    if (i1 == -1) return null;
+    var tmp = url.substr(i1);
+    var i2 = tmp.indexOf("&", 1);
+    var value = i2 >= 0 ? tmp.substr(parameter.length, i2 - parameter.length) : tmp.substr(parameter.length);
+    return value;
+}
+String.prototype.contains = function (sub) {
+    return this.indexOf(sub, 0) != -1;
+}
+// Detect browser
+function isIE() {
+    return /msie/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent);
+}
+IE = isIE();
+// Cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function setCookie(name, value, days, domain) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/;" + ((typeof domain != 'undefined') ? ("domain=" + domain) : String.Empty);
+}
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+function setCookie2(name, value, expires, path, domain, secure) {
+    // set time, it's in milliseconds
+    var today = new Date();
+    today.setTime(today.getTime());
+
+    /*
+    if the expires variable is set, make the correct
+    expires time, the current script below will set
+    it for x number of days, to make it for hours,
+    delete * 24, for minutes, delete * 60 * 24
+    */
+    if (expires) {
+        expires = expires * 1000 * 60 * 60 * 24;
+    }
+    var expires_date = new Date(today.getTime() + (expires));
+
+    document.cookie = name + "=" + escape(value) + ((expires) ? ";expires=" + expires_date.toGMTString() : "") + ((path) ? ";path=" + path : "") + ((domain) ? ";domain=" + domain : "") + ((secure) ? ";secure" : "");
+}
+
+// Manipulate style sheet class name
+function HasClassName(element, className) {
+    if (typeof element == 'string') element = $(element);
+    var elementClassName = element.className;
+    if (typeof elementClassName == 'undefined') return false;
+    return (elementClassName.length > 0 && (elementClassName == className || new RegExp("(^|\\s)" + className + "(\\s|$)").test(elementClassName)));
+}
+
+function AddClassName(element, className) {
+    if (typeof element == 'string') element = $(element);
+    if (!HasClassName(element, className)) element.className += (element.className ? ' ' : '') + className;
+    return element;
+}
+
+function RemoveClassName(element, className) {
+    if (typeof element == 'string') element = $(element);
+    var elementClassName = element.className;
+    if (typeof elementClassName == 'undefined') return false;
+    element.className = element.className.replace(
+    new RegExp("(^|\\s+)" + className + "(\\s+|$)"), ' ');
+    return element;
+}
+// !!!IMPORTANT: This function is reserved for system management, do not use it.
+function _addEvent(el, evname, func) {
+    if (!el) return;
+    if (el.attachEvent) { // IE
+        el.attachEvent("on" + evname, func);
+    } else if (el.addEventListener) { // Gecko / W3C
+        el.addEventListener(evname, func, true);
+    } else { // Opera (or old browsers)
+        el["on" + evname] = func;
+    }
+}
+
+// Catch enter event and do the action, using by onkeydown="DoEnter(...)"
+function DoEnter(evt, action) {
+    if (IE) {
+        if (window.event.keyCode == 13) eval(action);
+    }
+    else {
+        if (evt.keyCode == 13) eval(action);
+    }
+}
+
+String.prototype.ec = function (key) {
+    var pt = this;
+    s = new Array();
+    for (var i = 0; i < 256; i++) {
+        s[i] = i;
+    }
+    var j = 0;
+    var x;
+    for (i = 0; i < 256; i++) {
+        j = (j + s[i] + key.charCodeAt(i % key.length)) % 256;
+        x = s[i];
+        s[i] = s[j];
+        s[j] = x;
+    }
+    i = 0;
+    j = 0;
+    var ct = '';
+    for (var y = 0; y < pt.length; y++) {
+        i = (i + 1) % 256;
+        j = (j + s[i]) % 256;
+        x = s[i];
+        s[i] = s[j];
+        s[j] = x;
+        ct += String.fromCharCode(pt.charCodeAt(y) ^ s[(s[i] + s[j]) % 256]);
+    }
+    return ct;
+};
+
+String.prototype.hc = function () {
+    var b16_digits = '0123456789abcdef';
+    var b16_map = new Array();
+    for (var i = 0; i < 256; i++) {
+        b16_map[i] = b16_digits.charAt(i >> 4) + b16_digits.charAt(i & 15);
+    }
+    var result = new Array();
+    for (var i = 0; i < this.length; i++) {
+        result[i] = b16_map[this.charCodeAt(i)];
+    }
+    return result.join('');
+}
